7c6be2fd1fcaab79bcbd050c68f01b8093129bf0
Zhe <zarmour@foxmail.com>
2023-09-14 09:22:03 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueMacros from 'unplugin-vue-macros/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VueMacros({
+      plugins: {
+        vue: vue(),
+        // vueJsx: VueJsx(), // 如果需要
+      },
+    }),
+  ],
+})
